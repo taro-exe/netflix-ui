32d1a8a05f05f94e3aba5559460d10a0aa55745c
Rajiv Taro <rajivtarox@gmail.com>
2022-09-14 02:48:00 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const trailer = document.getElementById('trailer');
+
+function watchTrailer()
+{
+    trailer.style.visibility = 'visible';
+}
+
+function closeTrailer()
+{
+    trailer.style.visibility = 'hidden';
+}
+
+const movie = document.getElementById('movie');
+
+function watchMovie()
+{
+    movie.style.visibility = 'visible';
+}
+
+function closeMovie()
+{
+    movie.style.visibility = 'hidden';
+}
+
+
+
+
+
